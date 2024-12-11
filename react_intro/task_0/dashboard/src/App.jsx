6b21eb109e7580bce644c803064376b278957e54
Eidal559 <eidaljuarbe@gmail.com>
2024-12-10 22:23:02 -0400

    fix

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import holbertonLogo from './assets/holberton-logo.jpg';
 
 function App() {
   return (
-    <div>
-      <div className="App-header">
-        <img src={holbertonLogo} alt="holberton logo" />
-        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
+    <div className="App">
+        <div className="App-header">
+        <img src={logo} alt="logo" />
+        <h1>School dashboard</h1>
       </div>
       <div className="App-body">
-        <p>Login to access the full dashboard</p>
+        <p>
+          Login to access the full dashboard
+        </p>
       </div>
       <div className="App-footer">
-        <p>Copyright 2024 - Holberton School</p>
+        <p>Copyright 2020 - holberton School</p>
       </div>
     </div>
   );
