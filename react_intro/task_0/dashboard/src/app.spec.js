84e1804202be1284f1a2a78ee28f3a658716f0dc
Eidal559 <eidaljuarbe@gmail.com>
2024-12-21 22:52:01 -0400

    fix

--- react_intro/task_0/dashboard/src/app.spec.js
+++ react_intro/task_0/dashboard/src/app.spec.js
@@ -7,7 +7,7 @@ test('renders the h1 element with the text "School Dashboard"', () => {
     const headingElement = screen.getByRole('heading', { name: /school dashboard/i });
     expect(headingElement).toBeInTheDocument();
   });
-  
+
 
 test('checks text content in app-body and app-footer', () => {
     render(<App />);
@@ -17,11 +17,10 @@ test('checks text content in app-body and app-footer', () => {
     const footerText = screen.getByText(/copyright 2024 - holberton school/i);
     expect(footerText).toBeInTheDocument();
   });  
-  
+
 
 test('checks if an img element with alt text "holberton logo" is rendered', () => {
     render(<App />);
     const imgElement = screen.getByAltText(/holberton logo/i);
     expect(imgElement).toBeInTheDocument();
   });
-    
